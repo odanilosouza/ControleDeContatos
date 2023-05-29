eddb8432246a5f28c66b5ba0bb24a9c4004b8fd6
Danilo Souza <dev.danilosouza@gmail.com>
2023-05-28 21:56:06 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ControleDeContatos.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string email { get; set; }
+        public string Celular { get; set; }
+    }
+}
